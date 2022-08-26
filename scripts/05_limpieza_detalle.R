01ac5630bbb8bc3a70d64d7e85f1a74712ec2404
Saryace <saryace@gmail.com>
2022-08-26 12:35:46 -0400

    add mes y anio

--- scripts/05_limpieza_detalle.R
+++ scripts/05_limpieza_detalle.R
@@ -1,8 +1,10 @@
 
 # Hay que arreglar las classes chr a numeric ------------------------------
 
-datos_limpios_numericos <- 
-datos_limpios %>%
-  mutate(
-    valor_promedio = as.numeric(str_replace(valor_promedio,",","."))
-  )
+datos_limpios_numericos <-
+  datos_limpios %>%
+  mutate(valor_promedio = as.numeric(str_replace(valor_promedio, ",", "."))) %>%
+  # Hay que detallar mes y año de la medicion -------------------------------
+  mutate(fecha = str_remove(nombre_link, "Detalle por servicios - ")) %>% 
+  separate(fecha, c("mes","anio"), sep = " ")
+  
